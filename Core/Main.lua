df475d6925f5eb3d2ea9b2ee94d3a825d9decd54
Dale Hunt <dalehunt@gmail.com>
2025-05-11 13:53:18 +0100

    Emptied default whitelist/blacklists.

--- Core/Main.lua
+++ Core/Main.lua
@@ -11,11 +11,7 @@ UUF.Defaults = {
         AuraFilters = {
             Buffs = {
                 Whitelist = {},
-                Blacklist = {
-                    [440837] = true, -- "Fury of Xuen"
-                    [415603] = true, -- "Encapsulated Destiny"
-                    [404468] = true, -- "Flight Style: Steady"
-                },
+                Blacklist = {},
             },
             Debuffs = {
                 Whitelist = {},
