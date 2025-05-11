cab2e7bf944d24519b640ef90b3b952bf727ef3d
Dale <95256492+DaleHuntGB@users.noreply.github.com>
2025-05-11 16:07:44 +0100

    Added a variety of more useful IDs.

--- Core/Filters.lua
+++ Core/Filters.lua
@@ -1,12 +1,23 @@
 local _, UUF = ...
 
 local DebuffBlacklist = {
+    [57723] = true, -- Exhaustion
+    [390435] = true, -- Exhaustion
+    [264689] = true, -- Fatigued
+    [57724] = true, -- Sated
+    [95809] = true, -- Sated
+    [206151] = true, -- Challenger's Burden
+    [392960] = true, -- Demonic Gateway
 }
 
 local BuffBlacklist = {
-    [440837] = true, -- "Fury of Xuen"
-    [415603] = true, -- "Encapsulated Destiny"
-    [404468] = true, -- "Flight Style: Steady"    
+    [440837] = true, -- Fury of Xuen
+    [440839] = true, -- Kindness of Chi-Ji
+    [440836] = true, -- Essence of Yu'lon
+    [440838] = true, -- Fortitude of Niuzao
+    [415603] = true, -- Encapsulated Destiny
+    [404468] = true, -- Flight Style: Steady
+    [404464] = true, -- Flight Style: Skyriding
 }
 
 function UUF:FetchDebuffBlacklist()
