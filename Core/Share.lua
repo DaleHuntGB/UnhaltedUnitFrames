5ce7c09f27da32dafd8940abbd65ecbd03a14a14
Dale Hunt <dalehunt@gmail.com>
2025-07-28 10:40:27 +0100

    verbose checking for old profile imports, support new export functions

--- Core/Share.lua
+++ Core/Share.lua
@@ -16,9 +16,11 @@ end
 function UUF:ImportSavedVariables(EncodedInfo)
     local DecodedInfo = Compress:DecodeForPrint(EncodedInfo)
     local DecompressedInfo = Compress:DecompressDeflate(DecodedInfo)
-    local InformationDecoded, InformationTable = Serialize:Deserialize(DecompressedInfo)
-
-    if not InformationDecoded then print("Failed to import: invalid or corrupted string.") return end
+    local success, data = Serialize:Deserialize(DecompressedInfo)
+    if not success or type(data) ~= "table" then
+        print("Failed to import: invalid or corrupted string.")
+        return
+    end
 
     StaticPopupDialogs["UUF_IMPORT_PROFILE_NAME"] = {
         text = "Enter A Profile Name:",
@@ -31,18 +33,33 @@ function UUF:ImportSavedVariables(EncodedInfo)
         preferredIndex = 3,
         OnAccept = function(self)
             local newProfileName = self.EditBox:GetText() or self.editBox:GetText()
-            if newProfileName and newProfileName ~= "" then
-                UUF.DB:SetProfile(newProfileName)
+            if not newProfileName or newProfileName == "" then
+                print("Please enter a valid profile name.")
+                return
+            end
 
-                wipe(UUF.DB.profile)
-                for key, value in pairs(InformationTable.profile) do
+            UUF.DB:SetProfile(newProfileName)
+            wipe(UUF.DB.profile)
+
+            -- Handle Legacy Strings
+            -- Basically, check for the flatter structure.
+            if data.Player or data.Target then
+                for key, value in pairs(data) do
+                    if key ~= "global" then
+                        UUF.DB.profile[key] = value
+                    end
+                end
+                print("|cFF8080FFUnhalted|r Unit Frames: You have imported a legacy profile. Some options may not be configured correctly.")
+            elseif type(data.profile) == "table" then
+                for key, value in pairs(data.profile) do
                     UUF.DB.profile[key] = value
                 end
-                for key, value in pairs(InformationTable.global) do
+            end
+
+            if type(data.global) == "table" then
+                for key, value in pairs(data.global) do
                     UUF.DB.global[key] = value
                 end
-            else
-                print("Please enter a valid profile name.")
             end
         end,
     }
@@ -50,13 +67,15 @@ function UUF:ImportSavedVariables(EncodedInfo)
     StaticPopup_Show("UUF_IMPORT_PROFILE_NAME")
 end
 
+
+
 function UUFG:ExportUUF(profileKey)
     local profile = UUF.DB.profiles[profileKey]
     if not profile then return nil end
 
     local profileData = {
-        global = profile.global,
-        profile = profile.profile,
+        profile = profile,
+        global = UUF.DB.global,
     }
 
     local SerializedInfo = Serialize:Serialize(profileData)
@@ -70,7 +89,22 @@ function UUFG:ImportUUF(importString, profileKey)
     local DecompressedInfo = Compress:DecompressDeflate(DecodedInfo)
     local success, profileData = Serialize:Deserialize(DecompressedInfo)
 
-    if success and type(profileData.profile) == "table" then
+    if not success or type(profileData) ~= "table" then
+        print("Failed to import: invalid or corrupted string.")
+        return
+    end
+
+    -- Handle Legacy Strings
+    -- Basically, check for the flatter structure.
+    if profileData.Player or profileData.Target then
+        UUF.DB.profiles[profileKey] = profileData
+        UUF.DB:SetProfile(profileKey)
+        print("|cFF8080FFUnhalted|r Unit Frames: You have imported a legacy profile. Some options may not be configured correctly.")
+        return
+    end
+
+
+    if type(profileData.profile) == "table" then
         UUF.DB.profiles[profileKey] = profileData.profile
         UUF.DB:SetProfile(profileKey)
     end
