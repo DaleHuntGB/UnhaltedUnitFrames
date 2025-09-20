edf580293a5f0eaac9811441edf83a79d0d6c958
Dale Hunt <dalehunt@gmail.com>
2025-09-20 14:23:19 +0100

    Bunch of raid frame updates for better sorting

--- Units/Raid.lua
+++ Units/Raid.lua
@@ -2,10 +2,6 @@ local _, UUF = ...
 local oUF = UUF.oUF
 local CapitalizedUnits = UUF.CapitalizedUnits
 
-local _, UUF = ...
-local oUF = UUF.oUF
-local CapitalizedUnits = UUF.CapitalizedUnits
-
 function UUF:SpawnRaidFrames()
     local unit  = "raid"
     local DB    = UUF.db.profile[unit]
@@ -16,46 +12,38 @@ function UUF:SpawnRaidFrames()
     end)
     oUF:SetActiveStyle("UUF_" .. CapitalizedUnits[unit])
 
-    -- build group filter string dynamically
     local groups = {}
     for i = 1, Frame.GroupsToShow do
         groups[#groups+1] = tostring(i)
     end
     local groupString = table.concat(groups, ",")
 
-    -- decide unit growth (rows)
-    local point, xOffset, yOffset
-    if Frame.RowGrowth == "UP" then
-        point, xOffset, yOffset = "BOTTOM", 0, Frame.Spacing
-    elseif Frame.RowGrowth == "DOWN" then
+    local point, xOffset, yOffset, columnAnchorPoint
+    if Frame.GrowthDirection == "RIGHT_UP" then
         point, xOffset, yOffset = "TOP", 0, -Frame.Spacing
-    elseif Frame.RowGrowth == "LEFT" then
-        point, xOffset, yOffset = "RIGHT", -Frame.Spacing, 0
-    else -- RIGHT
-        point, xOffset, yOffset = "LEFT", Frame.Spacing, 0
-    end
-
-    -- decide column growth
-    local columnAnchorPoint
-    if Frame.ColumnGrowth == "LEFT" then
-        columnAnchorPoint = "LEFT"
-    elseif Frame.ColumnGrowth == "RIGHT" then
         columnAnchorPoint = "RIGHT"
-    elseif Frame.ColumnGrowth == "UP" then
+    elseif Frame.GrowthDirection == "RIGHT_DOWN" then
+        point, xOffset, yOffset = "BOTTOM", 0, Frame.Spacing
+        columnAnchorPoint = "RIGHT"
+    elseif Frame.GrowthDirection == "UP_RIGHT" then
+        point, xOffset, yOffset = "RIGHT", Frame.Spacing, 0
         columnAnchorPoint = "TOP"
-    else -- DOWN
-        columnAnchorPoint = "BOTTOM"
+    elseif Frame.GrowthDirection == "UP_LEFT" then
+        point, xOffset, yOffset = "LEFT", -Frame.Spacing, 0
+        columnAnchorPoint = "TOP"
+    else
+        point, xOffset, yOffset = "BOTTOM", 0, Frame.Spacing
+        columnAnchorPoint = "RIGHT"
     end
 
     self.Raid = oUF:SpawnHeader(
         "UUF_Raid", nil, "raid",
         "showRaid", DB.Enabled,
         "showPlayer", Frame.ShowPlayer,
-        "groupBy", "GROUP",
+        "groupBy", Frame.GroupBy or "GROUP",
         "groupFilter", groupString,
         "groupingOrder", groupString,
         "maxColumns", Frame.GroupsToShow,
-        "unitsPerColumn", Frame.UnitsPerColumn,
         "point", point,
         "xOffset", xOffset,
         "yOffset", yOffset,
