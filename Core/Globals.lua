8785a6ea31327336d674c1daaf7ec983b667faed
Dale Hunt <dalehunt@gmail.com>
2026-01-19 10:59:27 +0000

    bro i wanna go outside & touch grass

--- Core/Globals.lua
+++ Core/Globals.lua
@@ -118,6 +118,24 @@ end
 function UUF:LoadCustomColours()
     local General = UUF.db.profile.General
 
+    local PowerTypesToString = {
+        [0] = "MANA",
+        [1] = "RAGE",
+        [2] = "FOCUS",
+        [3] = "ENERGY",
+        [6] = "RUNIC_POWER",
+        [8] = "LUNAR_POWER",
+        [11] = "MAELSTROM",
+        [13] = "INSANITY",
+        [17] = "FURY",
+        [18] = "PAIN"
+    }
+
+    for powerType, color in pairs(General.Colours.Power) do
+        local powerTypeString = PowerTypesToString[powerType]
+        if powerTypeString then oUF.colors.power[powerTypeString] = oUF:CreateColor(color[1], color[2], color[3]) end
+    end
+
     for powerType, color in pairs(General.Colours.Power) do
         if powerType then oUF.colors.power[powerType] = oUF:CreateColor(color[1], color[2], color[3]) end
     end
