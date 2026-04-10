afca6e0968d6fd74f9a20fcf8602b7936b61d3b6
Dale Hunt <dalehunt@gmail.com>
2026-04-10 09:26:01 +0100

    Party GUI fixing

--- Core/Config/GUI.lua
+++ Core/Config/GUI.lua
@@ -723,7 +723,7 @@ local function CreateFrameSettings(containerParent, unit, unitHasParent, updateC
         SpacingSlider:SetLabel("Frame Spacing")
         SpacingSlider:SetValue(FrameDB.Layout[5])
         SpacingSlider:SetSliderValues(-1, 100, 0.1)
-        SpacingSlider:SetRelativeWidth(0.33)
+        SpacingSlider:SetRelativeWidth(0.25)
         SpacingSlider:SetCallback("OnValueChanged", function(_, _, value) FrameDB.Layout[5] = value updateCallback() end)
         LayoutContainer:AddChild(SpacingSlider)
     end
