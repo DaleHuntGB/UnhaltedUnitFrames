4de4c94ad91e9ab136a897e1e1b5fd9f5c214354
Dale Hunt <dalehunt@gmail.com>
2026-06-15 17:28:07 +0100

    Quest: Add Texture and then make adjustments for the new texture.

--- Elements/Indicators/Quest.lua
+++ Elements/Indicators/Quest.lua
@@ -1,11 +1,23 @@
 local _, UUF = ...
 
+local function UpdateQuestTexture(QuestIndicator)
+    local QuestIndicatorDB = UUF.db.profile.Units.target.Indicators.Quest
+    QuestIndicator:SetTexture(UUF.QuestTextures[QuestIndicatorDB.Texture] or UUF.QuestTextures.DEFAULT)
+    if QuestIndicatorDB.Texture == "QUEST0" then
+        QuestIndicator:SetHeight(QuestIndicatorDB.Size)
+        QuestIndicator:SetWidth(QuestIndicatorDB.Size * 0.35)
+    else
+        QuestIndicator:SetSize(QuestIndicatorDB.Size, QuestIndicatorDB.Size)
+    end
+end
+
 function UUF:CreateUnitQuestIndicator(unitFrame, unit)
     local QuestIndicatorDB = UUF.db.profile.Units[UUF:GetNormalizedUnit(unit)].Indicators.Quest
 
     local QuestIndicator = unitFrame.HighLevelContainer:CreateTexture(UUF:FetchFrameName(unit) .. "_QuestIndicator", "OVERLAY")
-    QuestIndicator:SetSize(QuestIndicatorDB.Size, QuestIndicatorDB.Size)
     QuestIndicator:SetPoint(QuestIndicatorDB.Layout[1], unitFrame.HighLevelContainer, QuestIndicatorDB.Layout[2], QuestIndicatorDB.Layout[3], QuestIndicatorDB.Layout[4])
+    QuestIndicator.PostUpdate = UpdateQuestTexture
+    UpdateQuestTexture(QuestIndicator)
 
     if QuestIndicatorDB.Enabled then
         unitFrame.QuestUnitIndicator = QuestIndicator
@@ -28,7 +40,6 @@ function UUF:UpdateUnitQuestIndicator(unitFrame, unit)
 
         if unitFrame.QuestUnitIndicator then
             unitFrame.QuestUnitIndicator:ClearAllPoints()
-            unitFrame.QuestUnitIndicator:SetSize(QuestIndicatorDB.Size, QuestIndicatorDB.Size)
             unitFrame.QuestUnitIndicator:SetPoint(QuestIndicatorDB.Layout[1], unitFrame.HighLevelContainer, QuestIndicatorDB.Layout[2], QuestIndicatorDB.Layout[3], QuestIndicatorDB.Layout[4])
             unitFrame.QuestUnitIndicator:ForceUpdate()
         end
