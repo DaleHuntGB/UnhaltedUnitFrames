9cd45deab2b439f4345e16bb9cefbdd2fe78b35c
Dale Hunt <dalehunt@gmail.com>
2026-06-19 09:39:25 +0100

    Defaults: Add Sorting Defaults.

--- Libraries/oUF_Elements/auras.lua
+++ Libraries/oUF_Elements/auras.lua
@@ -1,23 +1,24 @@
 local _, UUF = ...
 
-function UUF:ConfigureAuraSorting(container, sorting)
+function UUF:ConfigureAuraSorting(auraContainer, sorting)
 	sorting = sorting or "BLIZZARD"
-	if not container.UUFAuraSortingPatched then
-		container.UUFOriginalPostUpdateInfo = container.PostUpdateInfo
-		container.UUFAuraSortingPatched = true
+	if not auraContainer.UUFAuraSortingPatched then
+		auraContainer.UUFOriginalPostUpdateInfo = auraContainer.PostUpdateInfo
+		auraContainer.UUFAuraSortingPatched = true
 	end
 
 	if sorting == "BLIZZARD" or not C_UnitAuras.GetUnitAuraInstanceIDs then
-		container.PostUpdateInfo = container.UUFOriginalPostUpdateInfo
-		container.SortAuras = nil
-		container.UUFAuraSortOrder = nil
+		auraContainer.PostUpdateInfo = auraContainer.UUFOriginalPostUpdateInfo
+		auraContainer.SortAuras = nil
+		auraContainer.UUFAuraSortOrder = nil
 		return
 	end
 
-	container.UUFAuraSortRule = (sorting == "DURATION" or sorting == "DURATION_REVERSED") and Enum.UnitAuraSortRule.ExpirationOnly or Enum.UnitAuraSortRule.Default
-	container.UUFAuraSortDirection = (sorting == "BLIZZARD_REVERSED" or sorting == "DURATION_REVERSED") and Enum.UnitAuraSortDirection.Reverse or Enum.UnitAuraSortDirection.Normal
-	container.UUFAuraSortOrder = table.wipe(container.UUFAuraSortOrder or {})
-	container.PostUpdateInfo = function(element, unit, ...)
+	auraContainer.UUFAuraSortRule = (sorting == "DURATION" or sorting == "DURATION_REVERSED") and Enum.UnitAuraSortRule.ExpirationOnly or Enum.UnitAuraSortRule.Default
+	auraContainer.UUFAuraSortDirection = (sorting == "BLIZZARD_REVERSED" or sorting == "DURATION_REVERSED") and Enum.UnitAuraSortDirection.Reverse or Enum.UnitAuraSortDirection.Normal
+	auraContainer.UUFAuraSortOrder = table.wipe(auraContainer.UUFAuraSortOrder or {})
+
+	auraContainer.PostUpdateInfo = function(element, unit, ...)
 		if element.UUFOriginalPostUpdateInfo then element:UUFOriginalPostUpdateInfo(unit, ...) end
 		local filter = element.filter or "HELPFUL"
 		if type(filter) == "function" then filter = filter(element, unit) end
@@ -25,8 +26,9 @@ function UUF:ConfigureAuraSorting(container, sorting)
 		element.UUFAuraSortOrder = table.wipe(element.UUFAuraSortOrder or {})
 		for index, auraInstanceID in ipairs(auraInstanceIDs) do element.UUFAuraSortOrder[auraInstanceID] = index end
 	end
-	container.SortAuras = function(a, b)
-		local order = container.UUFAuraSortOrder
+
+	auraContainer.SortAuras = function(a, b)
+		local order = auraContainer.UUFAuraSortOrder
 		local orderA = order and order[a.auraInstanceID] or math.huge
 		local orderB = order and order[b.auraInstanceID] or math.huge
 		if orderA ~= orderB then return orderA < orderB end
